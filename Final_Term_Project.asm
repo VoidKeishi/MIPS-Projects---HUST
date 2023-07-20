3fd0f40fc84111d1575b5256709adfe2dcc72229
PhucPhuc <daosyphuc288@gmail.com>
2023-07-20 12:19:20 +0700

    Adding more features

--- Final_Term_Project.asm
+++ Final_Term_Project.asm
@@ -23,7 +23,23 @@
     frame_first_part_xor: .asciiz "[[ "
     frame_second_part_xor: .asciiz "]]"
     #Convert integer to hexadecimal ascii code
-    hexadecimal_code: .asciiz "00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F","10","11","12","13","14","15","16","17","18","19","1A","1B","1C","1D","1E","1F","20","21","22","23","24","25","26","27","28","29","2A","2B","2C","2D","2E","2F","30","31","32","33","34","35","36","37","38","39","3A","3B","3C","3D","3E","3F","40","41","42","43","44","45","46","47","48","49","4A","4B","4C","4D","4E","4F","50","51","52","53","54","55","56","57","58","59","5A","5B","5C","5D","5E","5F","60","61","62","63","64","65","66","67","68","69","6A","6B","6C","6D","6E","6F","70","71","72","73","74","75","76","77","78","79","7A","7B","7C","7D","7E","7F","80","81","82","83","84","85","86","87","88","89","8A","8B","8C","8D","8E","8F","90","91","92","93","94","95","96","97","98","99","9A","9B","9C","9D","9E","9F","A0","A1","A2","A3","A4","A5","A6","A7","A8","A9","AA","AB","AC","AD","AE","AF","B0","B1","B2","B3","B4","B5","B6","B7","B8","B9","BA","BB","BC","BD","BE","BF","C0","C1","C2","C3","C4","C5","C6","C7","C8","C9","CA","CB","CC","CD","CE","CF","D0","D1","D2","D3","D4","D5","D6","D7","D8","D9","DA","DB","DC","DD","DE","DF","E0","E1","E2","E3","E4","E5","E6","E7","E8","E9","EA","EB","EC","ED","EE","EF","F0","F1","F2","F3","F4","F5","F6","F7","F8","F9","FA","FB","FC","FD","FE","FF"
+    hexadecimal_code: .asciiz 
+    "00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F",
+    "10","11","12","13","14","15","16","17","18","19","1A","1B","1C","1D","1E","1F",
+    "20","21","22","23","24","25","26","27","28","29","2A","2B","2C","2D","2E","2F",
+    "30","31","32","33","34","35","36","37","38","39","3A","3B","3C","3D","3E","3F",
+    "40","41","42","43","44","45","46","47","48","49","4A","4B","4C","4D","4E","4F",
+    "50","51","52","53","54","55","56","57","58","59","5A","5B","5C","5D","5E","5F",
+    "60","61","62","63","64","65","66","67","68","69","6A","6B","6C","6D","6E","6F",
+    "70","71","72","73","74","75","76","77","78","79","7A","7B","7C","7D","7E","7F",
+    "80","81","82","83","84","85","86","87","88","89","8A","8B","8C","8D","8E","8F",
+    "90","91","92","93","94","95","96","97","98","99","9A","9B","9C","9D","9E","9F",
+    "A0","A1","A2","A3","A4","A5","A6","A7","A8","A9","AA","AB","AC","AD","AE","AF",
+    "B0","B1","B2","B3","B4","B5","B6","B7","B8","B9","BA","BB","BC","BD","BE","BF",
+    "C0","C1","C2","C3","C4","C5","C6","C7","C8","C9","CA","CB","CC","CD","CE","CF",
+    "D0","D1","D2","D3","D4","D5","D6","D7","D8","D9","DA","DB","DC","DD","DE","DF",
+    "E0","E1","E2","E3","E4","E5","E6","E7","E8","E9","EA","EB","EC","ED","EE","EF",
+    "F0","F1","F2","F3","F4","F5","F6","F7","F8","F9","FA","FB","FC","FD","FE","FF"
     
 .text
 prompt_to_enter:
@@ -107,7 +123,7 @@ working_with_input:
     sw $t2, second_half
     addi $t9, $t9, 8
     and $s1, $t1, 0xFF
-    beq $s1, '\n', reset
+    beq $s1, '\n', print_Downline
     
 working_with_xor:
     xor $t3, $t1, $t2
@@ -171,8 +187,10 @@ if_t8_equal_3:
     la $a0, frame_first_part_xor
     syscall
 
-    la $t8, convert
-    lw $t7, xor_part1
+    la $t8, hexadecimal_code
+    lb $t7, xor_part4
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
     add $t8, $t8, $t7
     li $v0, 4
     add $a0, $zero, $t8
@@ -182,8 +200,10 @@ if_t8_equal_3:
     la $a0, comma
     syscall
 
-    la $t8, convert
-    lw $t7, xor_part2
+    la $t8, hexadecimal_code
+    lb $t7, xor_part3
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
     add $t8, $t8, $t7
     li $v0, 4
     add $a0, $zero, $t8
@@ -193,8 +213,10 @@ if_t8_equal_3:
     la $a0, comma
     syscall
     
-    la $t8, convert
-    lw $t7, xor_part3
+    la $t8, hexadecimal_code
+    lb $t7, xor_part2
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
     add $t8, $t8, $t7
     li $v0, 4
     add $a0, $zero, $t8
@@ -204,8 +226,10 @@ if_t8_equal_3:
     la $a0, comma
     syscall
 
-    la $t8, convert
-    lw $t7, xor_part4
+    la $t8, hexadecimal_code
+    lb $t7, xor_part1
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
     add $t8, $t8, $t7
     li $v0, 4
     add $a0, $zero, $t8
@@ -222,6 +246,7 @@ if_t8_equal_3:
     subi	$t6, $t6, 1			# $t8 = $t8 - 1
     j working_with_input
 
+#################################################
 if_t8_equal_2:
     li $v0, 4
     la $a0, frame_first_part_data
@@ -243,36 +268,60 @@ if_t8_equal_2:
     la $a0, frame_first_part_xor
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part1
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part1
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part2
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part2
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
     
+    la $t8, hexadecimal_code
+    lb $t7, xor_part3
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part3
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part4
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part4
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, frame_second_part_xor
+    syscall
+
+	li $v0, 4
+    la $a0, space_between_column
     syscall
 #################################################
     
@@ -288,10 +337,6 @@ if_t8_equal_2:
     la $a0, frame_second_part_data
     syscall
 
-    li $v0, 4
-    la $a0, space_between_column
-    syscall
-
     li $v0, 11
     li $a0, '\n'
     syscall
@@ -305,37 +350,62 @@ if_t8_equal_1:
     la $a0, frame_first_part_xor
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part1
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part1
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part2
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part2
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
     
+    la $t8, hexadecimal_code
+    lb $t7, xor_part3
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part3
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, comma
     syscall
 
+    la $t8, hexadecimal_code
+    lb $t7, xor_part4
+    add $s1, $t7, $t7
+    add $t7, $s1, $t7
+    add $t8, $t8, $t7
     li $v0, 4
-    lb $a0, xor_part4
+    add $a0, $zero, $t8
     syscall
 
     li $v0, 4
     la $a0, frame_second_part_xor
     syscall
+    
+    li $v0, 4
+    la $a0, space_between_column
+    syscall
+
 #################################################
     li $v0, 4
     la $a0, frame_first_part_data
@@ -377,6 +447,12 @@ if_t8_equal_1:
     j working_with_input
 #################################################
 
+print_Downline:
+	li $v0, 4
+    la $a0, Upperline
+    syscall
+    j reset
+   
 reset:
     #Erase data in all string
     la $s0, input
